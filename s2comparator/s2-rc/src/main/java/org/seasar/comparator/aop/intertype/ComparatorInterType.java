2fd482f65b81f475616b1dfab168f80ca8597a23
hiro-h <hiro-h@6e7e5f6e-2580-42e3-bc69-e87c49a8bfb8>
2008-03-01 15:52:18 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2comparator/trunk@9 6e7e5f6e-2580-42e3-bc69-e87c49a8bfb8

---
+++ s2comparator/s2-rc/src/main/java/org/seasar/comparator/aop/intertype/ComparatorInterType.java
@@ -0,0 +1,16 @@
+package org.seasar.comparator.aop.intertype;
+
+import javassist.CannotCompileException;
+import javassist.NotFoundException;
+
+import org.seasar.framework.aop.intertype.AbstractInterType;
+
+public class ComparatorInterType extends AbstractInterType {
+
+	@Override
+	protected void introduce() throws CannotCompileException, NotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
