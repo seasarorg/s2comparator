3654bdda3aad6f4631b72dc737bbe54dbbcf08fc
hiro-h <hiro-h@6e7e5f6e-2580-42e3-bc69-e87c49a8bfb8>
2008-03-01 15:04:35 +0000

    add several main packages and a delegator
    git-svn-id: https://www.seasar.org/svn/sandbox/s2comparator/trunk@5 6e7e5f6e-2580-42e3-bc69-e87c49a8bfb8

---
+++ s2comparator/s2-rc/src/main/java/org/seasar/s2comparator/comparator/ComparatorDelegator.java
@@ -0,0 +1,7 @@
+package org.seasar.s2comparator.comparator;
+
+import java.util.Comparator;
+
+public interface ComparatorDelegator extends Comparator {
+
+}
---
+++ s2comparator/s2-rc/src/main/java/org/seasar/s2comparator/comparator/impl/ComparatorDelegatorImpl.java
@@ -0,0 +1,11 @@
+package org.seasar.s2comparator.comparator.impl;
+
+import org.seasar.s2comparator.comparator.ComparatorDelegator;
+
+public class ComparatorDelegatorImpl implements ComparatorDelegator {
+
+	public int compare(Object o1, Object o2) {
+		return 0;
+	}
+
+}
